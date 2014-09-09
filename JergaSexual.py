df6f9e3e002cd831b0c50a7a2b1e8b665ab60888
Matías Cubero <matias.cubero@moove-it.com>
2014-09-08 22:26:57 -0300

    Agregada Funcionalidad para comunicarse con TreeTagger: Hay que incluir la carpeta cmd de treetagger en el path

--- JergaSexual.py
+++ JergaSexual.py
@@ -1,13 +1,22 @@
 
 import Feature
+import utils
+import tokenizacion
+import define
+import utils
 
 FEATURE_NAME="Jerga Sexual"
 
-class JergaSexual(Feature):
+class JergaSexual(Feature.Feature):
 	def __init__(self):
-		pass
-
+		self.palabrasSexuales = utils.obtenerDiccionario(define.PATH_DICCIONARIO_SEXUAL)
+		
 	def calcularFeature(self, tweet):
+
 		valorFeature = 0
+
+		tokens = tokenizacion.tokenizar(tweet.texto)
+
+		print tokens
 
 		tweet.features[FEATURE_NAME] = valorFeature
--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
-
+import subprocess
 
 
 def obtenerDiccionario(filename):
 	lines = [line.rstrip('\n') for line in open(filename)]
 	return lines
+
+def ejecutarComando(command):
+	p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	return p.stdout.readlines()
 
 
