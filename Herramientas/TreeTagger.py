ecce6b3cfda4e5c2d9c498ecdad44e8acb175f61
Matías Cubero <matias.cubero@moove-it.com>
2014-09-09 20:12:08 -0300

    Restructuracion del proyecto

---
+++ Herramientas/TreeTagger.py
@@ -0,0 +1,30 @@
+
+import utils
+import re
+
+class TreeTagger:
+
+	def __init__(self, texto):
+		command = 'echo "' + texto + '" |  tree-tagger-spanish'
+		resultado = utils.ejecutarComando(command)
+		self.tokens = []
+		for line in resultado:
+			matcheo = re.search('^(.*)\t(.*)\t(.*)\n', line)
+			if matcheo != None:
+				detalle = TokenTT()
+				detalle.token = matcheo.group(1)
+				detalle.tag = matcheo.group(2)
+				detalle.lemma = matcheo.group(3)
+
+				self.tokens.append(detalle)
+
+
+#Datatype
+class TokenTT:
+
+	def __init__(self):
+		self.token = ""
+		self.tag = ""
+		self.lemma = ""
+
+
