b8a552fb6890d2359980bc1b673e6e9fdba2228a
Matias Cubero <mcubero7@hotmail.com>
2014-09-10 15:02:50 -0300

    Agregado persistencia a Tweets

--- Tweet.py
+++ Tweet.py
@@ -1,3 +1,12 @@
+
+import MySQLdb
+
+DB_HOST = 'localhost' 
+DB_USER = 'pghumor' 
+DB_PASS = 'ckP8t/2l'
+DB_NAME = 'chistesdb'
+
+
 class Tweet:
 
 	def __init__(self):
@@ -10,3 +19,17 @@ class Tweet:
 		self.es_humor = False
 
 		self.features = {}
+
+
+	def persistir(self):
+		
+		datos = [DB_HOST, DB_USER, DB_PASS, DB_NAME]
+		conection = MySQLdb.connect(*datos)
+		cursor = conection.cursor()
+
+		cursor.execute(query)		
+			for key,value in self.features.items():
+				query = "INSERT INTO features VALUES (" + `self.id` + ",'" + key+ "'," + value + ") ON DUPLICATE KEY UPDATE valor_feature = " + value
+				cursor.execute(query)
+
+		conection.commit()
