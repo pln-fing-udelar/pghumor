0f7135d666f53b7fb0cdec24a96a0ad4e92b22f8
Santiago Castro <santi.1410@hotmail.com>
2014-09-13 19:41:04 -0300

    reorganización de carpetas

---
+++ clasificador/features/feature.py
@@ -0,0 +1,13 @@
+import abc
+
+class Feature:
+	__metaclass__ = abc.ABCMeta
+	
+	def __init__(self):
+		self.nombre = ""
+		self.descripcion = ""
+
+	@abc.abstractmethod
+	def calcularFeature(self, tweet):
+		"""Calcula la feature para el tweet y le asigna su valor al mismo"""
+		return
