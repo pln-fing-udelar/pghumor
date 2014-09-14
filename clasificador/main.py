cba1661e4f6fac530c87ae2be627dbb505f41dd5
Santiago Castro <santi.1410@hotmail.com>
2014-09-14 04:27:25 -0300

    el main ahora evalua y tira porcentajes. estan fallando las features al menos en mi pc, siempre tiran 0 (algunas o todas tiran que no tienen tokens)

--- clasificador/main.py
+++ clasificador/main.py
@@ -35,4 +35,66 @@ if __name__ == "__main__":
 
 	clasificador.fit(features_entrenamiento, grupos_entrenamiento)
 
-	#clasificador.predict(evaluacion[0].features.values())
+
+
+
+	clasificados_positivos = 0
+	son_positivos = 0
+
+	verdaderos_positivos = 0
+	falsos_positivos = 0
+
+	verdaderos_negativos = 0
+	falsos_negativos = 0
+
+	precision = 0
+	recall = 0
+	accuracy = 0
+
+	for tweet in evaluacion:
+		clasificacion_es_humor = clasificador.predict(tweet.features.values())
+
+		if clasificacion_es_humor:
+			clasificados_positivos += 1
+
+		if tweet.es_humor:
+			son_positivos += 1
+
+		if clasificacion_es_humor == tweet.es_humor:
+			accuracy += 1
+
+			if clasificacion_es_humor:
+				verdaderos_positivos += 1
+			else:
+				verdaderos_negativos += 1
+		else:
+			if clasificacion_es_humor:
+				falsos_positivos += 1
+			else:
+				falsos_negativos += 1
+
+	if clasificados_positivos == 0:
+		precision = 1.0
+	else:
+		precision = float(verdaderos_positivos)/clasificados_positivos
+
+	if son_positivos == 0:
+		recall = 1.0
+	else:
+		recall = float(verdaderos_positivos)/son_positivos
+
+	accuracy = float(accuracy)/len(evaluacion)
+
+	print('VP: ' + str(verdaderos_positivos))
+	print('FP: ' + str(falsos_positivos))
+	print('VN: ' + str(verdaderos_negativos))
+	print('FN: ' + str(falsos_negativos))
+	print
+	print('Matriz de confusión:')
+	print('\tP\tN')
+	print('P\t' + str(verdaderos_positivos) + '\t' + str(falsos_positivos))
+	print('N\t' + str(falsos_negativos) + '\t' + str(verdaderos_negativos))
+	print
+	print('Precision: ' + str(precision))
+	print('Recall: ' + str(recall))
+	print('Accuracy: ' + str(accuracy))
