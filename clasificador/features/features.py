78723dff63906b5e4c002c4972e1477682abfdd5
Santiago Castro <santi.1410@hotmail.com>
2014-09-16 16:36:45 -0300

    acepta argumentos para recalcular features

--- clasificador/features/features.py
+++ clasificador/features/features.py
@@ -7,13 +7,19 @@ import clasificador.features.primerapersona
 
 class Features:
 	def __init__(self):
-		self.features = [
-			clasificador.features.jergasexual.JergaSexual(),
-			clasificador.features.oov.OOV(),
-			clasificador.features.primerapersona.PrimeraPersona(),
-		]
+		self.features = {}
+		for feature in [ \
+				clasificador.features.jergasexual.JergaSexual(), \
+				clasificador.features.oov.OOV(), \
+				clasificador.features.primerapersona.PrimeraPersona(), \
+				]:
+			self.features[feature.nombre] = feature
 
 	def calcular_features(self, tweets):
-		for feature in self.features:
+		for feature in self.features.values():
 			for tweet in tweets:
 				feature.calcular_feature(tweet)
+
+	def calcular_feature(self, tweets, nombre_feature):
+		for tweet in tweets:
+			self.features[nombre_feature].calcular_feature(tweet)
