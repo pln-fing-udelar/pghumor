4028f51bef8d2bfbd396fa19932921bae828e377
Santiago Castro <santi.1410@hotmail.com>
2014-09-16 19:19:40 -0300

    cambio menor

--- clasificador/herramientas/define.py
+++ clasificador/herramientas/define.py
@@ -1,10 +1,5 @@
 from __future__ import absolute_import
 
-#Archivos
-PATH_DICCIONARIO_SEXUAL = 'DiccionarioSexual.txt'
-PATH_DICCIONARIO_ANIMAL = 'DiccionarioAnimal.txt'
-PATH_DICCIONARIO_PERSONAL = 'DiccionarioPersonales.txt'
-
 #Twitter API credentials
 consumer_key = "GoJjP7Xmj4kpxjttr8qJ9cLtC"
 consumer_secret = "PnbLJGXnJjsE9M97yhHXY2Oyj7ojcrcVulDGM2yQfS05NQjoNK"
