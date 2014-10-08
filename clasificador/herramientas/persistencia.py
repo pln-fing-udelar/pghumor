5573aa3338276a8fe2e036006843ed594be27dc8
Santiago Castro <santi.1410@hotmail.com>
2014-10-08 03:32:02 -0200

    Cargado de tweets más eficiente

--- clasificador/herramientas/persistencia.py
+++ clasificador/herramientas/persistencia.py
@@ -17,13 +17,38 @@ def cargar_tweets(**options):
 	if cargar_evaluacion:
 		where_cargar_evaluacion = ''
 	else:
-		where_cargar_evaluacion = ' evaluacion = 0 '
+		where_cargar_evaluacion = 'WHERE evaluacion = 0'
 
-	consulta = 'SELECT id_account, id_tweet, text_tweet, favorite_count_tweet, retweet_count_tweet, eschiste_tweet, ' \
-			   'name_account, followers_count_account, evaluacion, (SELECT COUNT(*) FROM votos AS V WHERE ' \
-			   'V.id_tweet = T.id_tweet) as votos, (SELECT COUNT(*) FROM votos AS V WHERE  V.id_tweet = T.id_tweet ' \
-			   'AND (V.voto = \'x\' OR V.voto = \'n\' )) AS votos_no_humor_u_omitido FROM tweets AS T NATURAL JOIN twitter_accounts WHERE ' \
-			   + where_cargar_evaluacion + ' HAVING ((votos > 0 AND votos_no_humor_u_omitido/votos <= 0.25) OR eschiste_tweet = 0)'
+	consulta = """
+	SELECT id_account,
+		   T.id_tweet,
+		   text_tweet,
+		   favorite_count_tweet,
+		   retweet_count_tweet,
+		   eschiste_tweet,
+		   name_account,
+		   followers_count_account,
+		   evaluacion,
+		   votos,
+		   votos_no_humor_u_omitido,
+		   eschiste_tweet
+	FROM   tweets AS T
+		   NATURAL JOIN twitter_accounts
+						LEFT JOIN (SELECT id_tweet,
+										  Count(*) AS votos,
+										  Sum(CASE
+												WHEN voto = 'x'
+													  OR voto = 'n' THEN 1
+												ELSE 0
+											  end) AS votos_no_humor_u_omitido
+								   FROM   votos
+								   GROUP  BY id_tweet) V
+							   ON ( V.id_tweet = T.id_tweet )
+	%s
+	HAVING ( ( votos > 0
+			   AND votos_no_humor_u_omitido / votos <= 0.25 )
+			  OR eschiste_tweet = 0 );
+	""" % where_cargar_evaluacion
 
 	cursor.execute(consulta)
 
@@ -43,10 +68,30 @@ def cargar_tweets(**options):
 
 		resultado[tw.id] = tw
 
-	consulta = 'SELECT id_tweet, nombre_feature, valor_feature, (SELECT COUNT(*) FROM votos AS V WHERE ' \
-			   'V.id_tweet = T.id_tweet) as votos, (SELECT COUNT(*) FROM votos AS V WHERE  V.id_tweet = T.id_tweet ' \
-			   'AND (V.voto = \'x\' OR V.voto = \'n\' )) AS votos_no_humor_u_omitido, eschiste_tweet FROM features NATURAL JOIN tweets AS T WHERE ' \
-			   + where_cargar_evaluacion + ' HAVING ((votos > 0 AND votos_no_humor_u_omitido/votos <= 0.25) OR eschiste_tweet = 0)'
+	consulta = """
+	SELECT T.id_tweet,
+		   nombre_feature,
+		   valor_feature,
+		   votos,
+		   votos_no_humor_u_omitido,
+		   eschiste_tweet
+	FROM   features
+		   NATURAL JOIN tweets AS T
+						LEFT JOIN (SELECT id_tweet,
+										  Count(*) AS votos,
+										  Sum(CASE
+												WHEN voto = 'x'
+													  OR voto = 'n' THEN 1
+												ELSE 0
+											  end) AS votos_no_humor_u_omitido
+								   FROM   votos
+								   GROUP  BY id_tweet) V
+							   ON ( V.id_tweet = T.id_tweet )
+	%s
+	HAVING ( ( votos > 0
+			   AND votos_no_humor_u_omitido / votos <= 0.25 )
+			  OR eschiste_tweet = 0 );
+    """ % where_cargar_evaluacion
 
 	cursor.execute(consulta)
 
