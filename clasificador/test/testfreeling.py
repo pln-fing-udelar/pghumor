9da33accac1dc57b865cfc6d17d7a915330f230d
Santiago Castro <santi.1410@hotmail.com>
2014-10-11 13:48:25 -0200

    Arreglado escapado de texto que va para la consola

---
+++ clasificador/test/testfreeling.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+import unittest
+
+import clasificador.features.preguntarespuesta
+from clasificador.herramientas.freeling import Freeling
+from clasificador.realidad.tweet import Tweet
+
+
+class TestFreeling(unittest.TestCase):
+    def test_freeling_escapar(self):
+        tweet = Tweet()
+        tweet.id = 58179039764021248
+        tweet.texto = u"3 Cosas que he aprendi en la escuela: Enviar WhatsApp's sin mirar. Dormir sin que me vean. El trabajo en equipo durante los ex\xe1menes."
+        tweet.texto_original = tweet.texto
+        tweet.favoritos = 3
+        tweet.retweets = 14
+        tweet.es_humor = 1
+        tweet.cuenta = 132679073
+
+        freeling = Freeling(tweet)
+        self.assertNotEqual(freeling.tokens, [], "Error de tokens vacíos")
+
+if __name__ == '__main__':
+    unittest.main()
