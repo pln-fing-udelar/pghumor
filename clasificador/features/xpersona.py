80a1fdb311ca80c11dd977e22eaf4e8d9fc9bac1
competenciasroboticas <mcubero7@gmail.com>
2014-12-10 16:17:53 -0200

    Agregada feature de segunda persona, reorganización del codigo para que sea más mantenible

---
+++ clasificador/features/xpersona.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+import math
+
+from clasificador.features.feature import Feature
+from clasificador.herramientas.freeling import Freeling
+
+
+def esta_en_persona(tag, numeroPersona):
+    # determinante en 'numeroPersona' persona
+    # OR verbo en primera persona
+    # OR pronombre en primera persona
+    return (tag[0] == 'D' and tag[2] == str(numeroPersona)) or\
+           (tag[0] == 'V' and tag[4] == str(numeroPersona)) or\
+           (tag[0] == 'P' and tag[2] == str(numeroPersona))
+
+
+class XPersona(Feature):
+    def __init__(self, persona):
+        super(XPersona, self).__init__()
+        self.persona = persona
+
+    def calcular_feature(self, tweet):
+        tf = Freeling(tweet)
+        primera_persona = 0
+        for token in tf.tokens:
+            if esta_en_persona(token.tag, self.persona):
+                primera_persona += 1
+
+        if len(tf.tokens) == 0:
+            print("Error de tokens vacíos en " + self.nombre + ": ", tweet.texto)
+            return 0
+        else:
+            return primera_persona / math.sqrt(len(tf.tokens))
