167740d30361a4494ad9549475aa3728bcd4b14b
Matías Cubero <matias.cubero@moove-it.com>
2014-12-21 02:10:19 -0200

    Agregadas herramientas para poder realizar enfoque tematico. Hay que agregar wiki corpus raw en clasificador/recursos/wikicorpus/raw/

---
+++ clasificador/realidad/chiste.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+__author__ = 'matiascubero'
+
+
+class Chiste:
+
+    def __init__(self):
+        self.id_chiste = -1
+        self.texto_chiste = ""
+        self.id_clasificacion = -1
+        self.nombre_clasificacion = ""
+        self.votacion = -1
+        self.cantidad_votantes = -1
