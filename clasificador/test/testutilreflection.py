f16b24e771a71c8db2fac754f4ae7778af272178
Santiago Castro <santi.1410@hotmail.com>
2014-12-27 00:19:02 -0200

    agrega test para utilreflection cambios menores también

---
+++ clasificador/test/testutilreflection.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+from __future__ import unicode_literals
+
+import unittest
+
+from clasificador.herramientas.utilreflection import paquete, subclases, modulos_vecinos
+
+
+class TestUtilReflection(unittest.TestCase):
+    def test_paquete_basico(self):
+        _paquete = paquete(__name__)
+        self.assertEquals('.', _paquete, "El nombre de paquete debería ser '.' en lugar de " + _paquete)
+
+    def test_subclases_basico(self):
+        self.assertTrue(TestUtilReflection in subclases(unittest.TestCase))
+
+    def test_modulos_vecinos_basico(self):
+        self.assertTrue('testantonimos' in modulos_vecinos(__file__))
+
+
+if __name__ == '__main__':
+    unittest.main()
