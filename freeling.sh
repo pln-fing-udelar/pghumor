1c04fd84eb38737596775f4ff3d6d15cd286d218
Santiago Castro <santi.1410@hotmail.com>
2014-12-27 13:54:03 -0200

    hace el script de freeling más silencioso

--- freeling.sh
+++ freeling.sh
@@ -2,8 +2,8 @@
 
 if [ $1 = "start" ]; then
     killall analyzer > /dev/null 2> /dev/null
-    analyze -f $FREELINGSHARE/config/es.cfg --noprob --outf morfo --server --workers 4 --queue 50 --port 11111 --flush & > /dev/null
-    analyze -f $FREELINGSHARE/config/es.cfg --server --workers 4 --queue 50 --port 55555 --flush & > /dev/null
+    analyze -f $FREELINGSHARE/config/es.cfg --noprob --outf morfo --server --workers 4 --queue 50 --port 11111 --flush > /dev/null 2> /dev/null &
+    analyze -f $FREELINGSHARE/config/es.cfg --server --workers 4 --queue 50 --port 55555 --flush > /dev/null 2> /dev/null &
 elif [ $1 = "stop" ]; then
     killall analyzer
 else
