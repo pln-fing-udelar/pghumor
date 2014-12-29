6ba1e4f99fae29e2dd78ce4b3cca076e395728bc
Santiago Castro <santi.1410@hotmail.com>
2014-12-29 00:25:21 -0200

    cambios menores

--- clasificador/herramientas/utils.py
+++ clasificador/herramientas/utils.py
@@ -1,14 +1,19 @@
 # coding=utf-8
 from __future__ import absolute_import, division, unicode_literals
 
-import subprocess
 import glob
-import xml.etree.cElementTree as ET
+import subprocess
+import xml.etree.cElementTree as elementTree
+
+from pkg_resources import resource_filename
 
 
 def obtener_diccionario(filename):
-    lines = [line.decode('utf-8').rstrip('\n') for line in open(filename)]
-    return lines
+    with open(filename) as archivo:
+        lineas = []
+        for linea in archivo:
+            lineas.append(linea.decode('utf-8').rstrip('\n'))
+        return lineas
 
 
 def ejecutar_comando(command):
@@ -16,40 +21,40 @@ def ejecutar_comando(command):
         try:
             p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             p.wait()
-            return [_str.decode('utf-8') for _str in p.stdout.readlines()]
+            return [linea.decode('utf-8') for linea in p.stdout.readlines()]
         except:
+            # FIXME: habría que tomar una excepción más específica, ya que
+            # no se puede salir por Ctrl + C del programa si está acá
             pass
 
 
 def read_wiki_corpus():
-    files = glob.glob("clasificador/recursos/wikicorpus/raw/*")
+    files = glob.glob(resource_filename('clasificador.recursos', 'wikicorpus/raw/*'))
     documentos = []
     for filename in files:
         documento = ""
         estado = 0
-        with open(filename) as f:
-            for line in f.readlines():
-                if line == '\n' or line == 'ENDOFARTICLE.\n':
+        with open(filename) as archivo:
+            for linea in archivo:
+                if linea == '\n' or linea == 'ENDOFARTICLE.\n':
                     continue
                 if estado == 0:
-                    if line.startswith("<doc"):
+                    if linea.startswith("<doc"):
                         documento = ""
                         estado = 1
                 else:
                     if estado == 1:
-                        if line.startswith("</doc>"):
+                        if linea.startswith("</doc>"):
                             estado = 0
                             documentos.append(documento)
                         else:
-                            documento += line.decode('latin-1')
-
+                            documento += linea.decode('latin-1')
     return documentos
 
 
 def obtener_sample_wikicorpus():
-    tree = ET.parse('clasificador/recursos/wikicorpus_sample.xml')
+    tree = elementTree.parse(resource_filename('clasificador.recursos', 'wikicorpus_sample.xml'))
     documentos = []
     for documento in tree.findall('documento'):
         documentos.append(documento.text)
-
     return documentos
