c2c2cdb78c576a9ae2ae34dbf3a817db613970ea
Santiago Castro <santi.1410@hotmail.com>
2015-01-01 02:54:39 -0200

    cambio menor

--- clasificador/test/testantonimos.py
+++ clasificador/test/testantonimos.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from __future__ import unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import unittest
 
@@ -11,7 +11,7 @@ class TestAntonimos(unittest.TestCase):
     def test_antonimos_basico(self):
         tweet = Tweet()
         tweet.id = 58179039764021248
-        tweet.texto = 'Un pequeño paso para el hombre, pero es un gran paso para la humanidad.'
+        tweet.texto = "Un pequeño paso para el hombre, pero es un gran paso para la humanidad."
         tweet.texto_original = tweet.texto
         tweet.favoritos = 3
         tweet.retweets = 14
@@ -22,7 +22,7 @@ class TestAntonimos(unittest.TestCase):
 
         tweet.features[antonimos.nombre] = antonimos.calcular_feature(tweet)
         self.assertEquals(0.25, tweet.features[antonimos.nombre],
-                          'El tweet debería tener 0.25 en antonimos, no ' + str(tweet.features[antonimos.nombre]))
+                          "El tweet debería tener 0.25 en antonimos, no " + str(tweet.features[antonimos.nombre]))
 
 
 if __name__ == '__main__':
