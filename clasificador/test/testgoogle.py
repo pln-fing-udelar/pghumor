6f19bf919a2b0710050ca7f8512b007cc0609969
Santiago Castro <santi.1410@hotmail.com>
2015-01-19 17:53:00 -0200

    mueve la consulta de google de oov a un archivo auxiliar y separa los tests de google en oov en otro archivo

---
+++ clasificador/test/testgoogle.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import unittest
+
+from clasificador.herramientas.google import Google
+
+
+class TestGoogle(unittest.TestCase):
+    def test_esta_en_google_palabra_comun(self):
+        texto = "hola"
+        self.assertTrue(Google.esta_en_google(texto), "Debería estar en google el texto \"" + texto + "\"")
+
+    def test_esta_en_google_palabra_comun_con_acento(self):
+        texto = "árbol"
+        self.assertTrue(Google.esta_en_google(texto), "Debería estar en google el texto \"" + texto + "\"")
+
+    def test_esta_en_google_error_de_tipeo(self):
+        texto = "holaa"
+        self.assertFalse(Google.esta_en_google(texto), "No debería estar en google el texto \"" + texto + "\"")
+
+    def test_esta_en_google_palabra_inexistente(self):
+        texto = "jajajajaaaaaaaa"
+        self.assertFalse(Google.esta_en_google(texto), "No debería estar en google el texto \"" + texto + "\"")
+
+    def test_esta_en_google_palabra_inexistente2(self):
+        texto = "aldnkvnvrbyweruvnrhuvhuirbv"
+        self.assertFalse(Google.esta_en_google(texto), "No debería estar en google el texto \"" + texto + "\"")
+
+
+if __name__ == '__main__':
+    unittest.main()
