e24e8175cca5dc958aeaffadfeba781bcfff400b
Pablo Duboue <pablo.duboue@gmail.com>
2016-12-05 04:12:32 -0500

    Deleted credentials

--- clasificador/config/environment.py
+++ clasificador/config/environment.py
@@ -4,13 +4,13 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os
 
 # Twitter API credentials
-os.environ['CONSUMER_KEY'] = 'GoJjP7Xmj4kpxjttr8qJ9cLtC'
-os.environ['CONSUMER_SECRET'] = 'PnbLJGXnJjsE9M97yhHXY2Oyj7ojcrcVulDGM2yQfS05NQjoNK'
-os.environ['ACCESS_KEY'] = '2714871673-HF7B4EPK4mWceAuEuBR4TRhJ12AGlJCVS6VPjZb'
-os.environ['ACCESS_SECRET'] = 'Yjp80IStjuot5Muvy4SAt2qoaHQdFGQDMJBqD4HQqX1s6'
+os.environ['CONSUMER_KEY'] = ''
+os.environ['CONSUMER_SECRET'] = ''
+os.environ['ACCESS_KEY'] = ''
+os.environ['ACCESS_SECRET'] = ''
 
 os.environ['DB_HOST'] = 'localhost'
 os.environ['DB_USER'] = 'pghumor'
-os.environ['DB_PASS'] = 'ckP8t/2l'
+os.environ['DB_PASS'] = ''
 os.environ['DB_NAME'] = 'corpus'
 os.environ['DB_NAME_CHISTES_DOT_COM'] = 'chistesdotcom'
